a66873842ea77a8a0c3853ec882b5311204a209b
LAPTOP-IPA0BIH5\Craig <craigplmr434@gmail.com>
2020-06-08 09:49:56 +0100

    293 Converted Spring Experiment to c++

---
+++ BattleTank/Source/BattleTank/Public/SprungWheel.h
@@ -0,0 +1,37 @@
+// Copyright Craig Palmer 2020
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SprungWheel.generated.h"
+
+class UPhysicsConstraintComponent;
+
+UCLASS()
+class BATTLETANK_API ASprungWheel : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASprungWheel();
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	//Static Mesh Root - Mass
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UStaticMeshComponent* Mass = nullptr;
+	//Physics Constraint Component
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UPhysicsConstraintComponent* MassWheelConstraint = nullptr;
+	//Static Mesh - Wheel
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UStaticMeshComponent* Wheel = nullptr;
+
+};
